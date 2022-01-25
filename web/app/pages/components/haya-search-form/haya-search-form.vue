2a7d80dbdd14e2192e7f3755537b08c7c2d60b48
tanxj <291067196@qq.com>
2022-01-25 21:40:49 +0800

    添加移动端分页显示

--- web/app/pages/components/haya-search-form/haya-search-form.vue
+++ web/app/pages/components/haya-search-form/haya-search-form.vue
@@ -42,9 +42,9 @@
 						<u-icon slot="suffix" name="arrow-right"></u-icon>
 					</u--input>
 				</u-form-item>
-				<u-form-item label="最大条数" name="limit">
+				<!-- <u-form-item label="最大条数" name="limit">
 					<u--input @confirm="submit" v-model="form.limit" placeholder="请输入最大显示条数" />
-				</u-form-item>
+				</u-form-item> -->
 				<u-form-item label="排序标准" name="orderBy" required @click="handleOpenSheet('orderBy')">
 					<u--input :value="getSheetLabel('orderBy', form.orderBy)" disabled :disabledColor="color.bgColor"
 						placeholder="请选择楼号">
@@ -87,6 +87,7 @@
 </template>
 
 <script>
+	import {buildingList} from '../../../api/common.js'
 	import color from '../../../lib/color.js'
 	export default {
 		name: 'haya-search-form',
@@ -115,8 +116,6 @@
 					order: 2,
 					// 仅获取json类型
 					type: 2,
-					// 最大显示条数
-					limit: 100,
 					fuzzy_key: ''
 				},
 				// 校验规则
@@ -128,12 +127,6 @@
 						}]
 					},
 					floor: {
-						rules: [{
-							format: 'number',
-							errorMessage: '楼层只能输入数字'
-						}]
-					},
-					limit: {
 						rules: [{
 							format: 'number',
 							errorMessage: '楼层只能输入数字'
@@ -153,30 +146,7 @@
 				actionsKey: 'no',
 				actionsMap: {
 					// 楼号集合
-					no: [{
-							name: '六号楼',
-							value: 6
-						},
-						{
-							name: '五号楼',
-							value: 5
-						},
-						{
-							name: '四号楼',
-							value: 4
-						},
-						{
-							name: '三号楼',
-							value: 3
-						},
-						{
-							name: '二号楼',
-							value: 2
-						},
-						{
-							name: '一号楼',
-							value: 1
-						},
+					no: [
 					],
 					// 评论类型集合
 					tag: [{
@@ -235,6 +205,33 @@
 			actions() {
 				return this.actionsMap[this.actionsKey]
 			}
+		},
+		mounted() {
+			buildingList().then(res=>{
+				let list = []
+				
+				for(let i = res.length - 1; i >= 0; i--){
+					let item = res[i]
+					item['value'] = item['building_no']
+					item['name'] = item['building_name'] + '号专楼'
+					list.push(item)
+				}
+				
+				this.actionsMap.no = list
+				if(!res || res.length === 0){
+					uni.showToast({
+						icon: 'none',
+						title: '未获取到专楼列表'
+					})
+				}
+				
+			}).catch(e=>{
+				console.log(e)
+				uni.showToast({
+					icon: 'none',
+					title: '未获取到专楼列表'
+				})
+			})
 		},
 		methods: {
 			handleClose() {
@@ -301,6 +298,10 @@
 			},
 			sheetSelect(e) {
 				this.form[this.actionsKey] = e.value
+				if(this.actionsKey == 'no'){
+					this.form.startDate = e.startDatetime
+					this.form.endDate = e.endDatetime
+				}
 			},
 			getSheetLabel(key, value) {
 				let list = this.actionsMap[key]
--- web/app/pages/index/index.vue
+++ web/app/pages/index/index.vue
@@ -7,19 +7,20 @@
 		<!-- 搜索表单 -->
 		<haya-search-form style="margin-top: 50px;" v-show="showSearch" @submit="submit"></haya-search-form>
 
-		<u-loading-page style="z-index: 999999;" :loading="loading"></u-loading-page>
+		<!-- <u-loading-page style="z-index: 999999;" :loading="loading"></u-loading-page> -->
 		<!-- 展示部分 -->
-		<view v-show="!showSearch" style="margin-top: 50px;" class="u-page">
-			<u-empty v-show="list.length === 0" mode="list"
+		<view v-show="!showSearch" style="margin-top: 50px; padding-bottom: 10px;" class="u-page">
+			<u-empty v-show="list.length === 0 && !loading" mode="list"
 				icon="http://cdn.uviewui.com/uview/empty/list.png">
 			</u-empty>
 			<view class="u-demo-block">
-				<view class="u-page u-demo-block__content">
+				<view>
 					<!-- 内容组件 -->
 					<haya-connents-item v-for="(item,index) in list" :item="item"
 						:bgColor="index % 2 === 0 ? color.bgColor : color.bgDark">
 					</haya-connents-item>
 				</view>
+				<u-loadmore :status="pageStatus" @loadmore="loadMore" />
 			</view>
 			<u-back-top ref="backTop" :scroll-top="scrollTop"></u-back-top>
 		</view>
@@ -50,10 +51,26 @@
 				// 标题
 				title: 'NGA专楼自助查询',
 				loading: false,
-				page: {},
+				page: {
+					pageNum: 1,
+					pageSize: 20,
+					hasMore: true
+				},
+				form: {},
 				list: [],
 
 				scrollTop: 0
+			}
+		},
+		computed:{
+			pageStatus(){
+				if(this.loading){
+					return 'loading'
+				}else if(this.page.hasMore){
+					return 'loadmore'
+				}else {
+					return 'nomore'
+				}
 			}
 		},
 		onPageScroll(e) {
@@ -61,19 +78,59 @@
 		},
 		methods: {
 			submit(form) {
-				this.loading = true
-				list(form).then(res => {
-					this.loading = false
-					this.list = res
+				this.form = form;
+				this.page = {
+					pageNum: 1,
+					pageSize: 20,
+					hasMore: true
+				}
+				this.showSearch = false
+				this.load().then((data) => {
+					this.list = data
 					this.$refs.backTop.backToTop()
-					this.showSearch = false
+					
+					// 处理分页数据
+					if(data.length > 0){
+						this.page.lastStartDate = data[data.length - 1].date
+						if(data.length === this.form.limit){
+							this.page.hasMore = true
+						}
+					}
 				}).catch(e => {
-					this.loading = false
+					this.form = {}
 					this.list = []
-					this.$emit('error', res)
 					console.log(e)
 				})
 			},
+			loadMore(){
+				this.page.pageNum += 1
+				this.load().then(data => {
+					this.list = this.list.concat(data)
+					// 处理分页数据
+					if(data.length > 0){
+						if(data.length < this.page.pageSize){
+							this.page.hasMore = false
+						}
+					}
+				}).catch(e => {
+					console.log(e)
+				})
+			},
+			load(){
+				return new Promise((res, rej) => {
+					this.loading = true
+					let form = JSON.parse(JSON.stringify(this.form))
+					form = Object.assign({}, form, this.page)
+					list(form).then(data => {
+						this.loading = false
+						res(data)
+					}).catch(e => {
+						this.loading = false
+						ref(e)
+					})
+				})
+			},
+			
 
 		}
 	}
