096359c36ad780d091d4af1f4b2cf2e4368b9689
FlowingCloudL <994288564@qq.com>
2022-01-25 12:07:54 +0800

    1.架构稍微整理了下 2.新增楼信息 3.接口调整

--- module-haya152/haya152-consumer-nga/src/main/java/com/fclProject/haya152/consumer/nga/controller/PageController.java
+++ module-haya152/haya152-consumer-nga/src/main/java/com/fclProject/haya152/consumer/nga/controller/PageController.java
@@ -1,11 +1,7 @@
 package com.fclProject.haya152.consumer.nga.controller;
 
-import com.fclProject.haya152.consumer.nga.dto.NgaDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 public class PageController {
@@ -15,9 +11,4 @@ public class PageController {
         return "select";
     }
 
-//    @RequestMapping("/show")
-//    public String showPage(List<NgaDto> list, Model model) {
-//        model.addAttribute("list",list);
-//        return "show";
-//    }
 }
--- module-haya152/haya152-consumer-nga/src/main/java/com/fclProject/haya152/consumer/nga/controller/SelectController.java
+++ module-haya152/haya152-consumer-nga/src/main/java/com/fclProject/haya152/consumer/nga/controller/SelectController.java
@@ -1,12 +1,15 @@
 package com.fclProject.haya152.consumer.nga.controller;
 
 import com.fclProject.haya152.consumer.nga.dao.NgaMapper;
-import com.fclProject.haya152.consumer.nga.dto.NgaDto;
+import com.fclProject.haya152.consumer.nga.dto.NgaBuildingDto;
+import com.fclProject.haya152.consumer.nga.dto.NgaCommentsDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,48 +20,46 @@ public class SelectController {
     @Autowired
     private NgaMapper ngaMapper;
 
-    @PostMapping("/select/default")
-    public String select(Integer no, Integer uid, String name, Integer floor, String fuzzy_key, Integer tag,
-                               String startDate, String endDate, String orderBy, Integer order, Integer limit,
-                               Integer type, Model model) {
-        if (type == 2) return "forward:/select";
+    @RequestMapping("/select/comments")
+    public String selectComments(Integer no, Integer uid, String name, Integer floor, String fuzzy_key, Integer tag,
+                         String startDate, String endDate, String orderBy, Integer order, Integer limit,
+                         Integer type, HttpServletRequest request) {
         Map<String,Object> map = new HashMap<>();
-        map.put("no","nga0"+no);
+        map.put("no",ngaMapper.selectBuildingTableName(no));
+        System.out.println(map.get("no"));
         if (uid != null) map.put("uid",uid);
         if (!name.equals("")) map.put("name",name);
         if (floor != null) map.put("floor",floor);
         if (fuzzy_key != null) map.put("fuzzy_key",fuzzy_key);
         if (tag != null) map.put("tag",tag);
-        if (!startDate.equals("")) map.put("startDate",startDate);
-        if (!endDate.equals("")) map.put("endDate",endDate);
+        if (!startDate.equals("")) map.put("startDatetime",startDate);
+        if (!endDate.equals("")) map.put("endDatetime",endDate);
         if (!orderBy.equals("")) map.put("orderBy",orderBy);
         if (order != null) map.put("order",order);
         if (limit != null) map.put("limit",limit);
         else map.put("limit",100);
-        List<NgaDto> list = ngaMapper.selectByDynamicCriteria(map);
-        model.addAttribute("list",list);
-        return "show";
+        List<NgaCommentsDto> list = ngaMapper.selectCommentsByDynamicCriteria(map);
+        request.setAttribute("list",list);
+        if (type == 2) return "forward:/select/comments/json";
+        else return "show";
     }
 
-    @PostMapping ("/select")
+    @PostMapping ("/select/comments/json")
     @ResponseBody
-    public List<NgaDto> select(Integer no, Integer uid, String name, Integer floor, String fuzzy_key, Integer tag,
-                         String startDate, String endDate, String orderBy, Integer order, Integer limit) {
-        Map<String,Object> map = new HashMap<>();
-        map.put("no","nga0"+no);
-        if (uid != null) map.put("uid",uid);
-        if (!name.equals("")) map.put("name",name);
-        if (floor != null) map.put("floor",floor);
-        if (fuzzy_key != null) map.put("fuzzy_key",fuzzy_key);
-        if (tag != null) map.put("tag",tag);
-        if (!startDate.equals("")) map.put("startDate",startDate);
-        if (!endDate.equals("")) map.put("endDate",endDate);
-        if (!orderBy.equals("")) map.put("orderBy",orderBy);
-        if (order != null) map.put("order",order);
-        if (limit != null) map.put("limit",limit);
-        else map.put("limit",100);
-        List<NgaDto> list = ngaMapper.selectByDynamicCriteria(map);
-        return list;
+    public List<NgaCommentsDto> selectComments(HttpServletRequest request) {
+        return (List<NgaCommentsDto>) request.getAttribute("list");
+    }
+
+    @RequestMapping("/select/building/{no}")
+    @ResponseBody
+    public NgaBuildingDto selectBuilding(@PathVariable("no") Integer no) {
+        return ngaMapper.selectBuildingByNo(no);
+    }
+
+    @RequestMapping("/select/building/list")
+    @ResponseBody
+    public List<NgaBuildingDto> selectBuilding() {
+        return ngaMapper.selectBuildingList();
     }
 
 }
